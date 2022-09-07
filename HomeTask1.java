cac45bd65ae8e7bc0836988b0300603c114077cd
Geork <112654321+jo777ko@users.noreply.github.com>
2022-09-07 14:22:15 +0200

    Add files via upload

---
+++ HomeTask1.java
@@ -0,0 +1,12 @@
+public class HomeTask1 {
+    public static void main(String[] args) {
+	int a = 5;
+	int b = 4;
+	int S = a * b;
+	int P = (a + b) * 2;
+	System.out.println(S);
+	System.out.println(P);
+	}
+}
+
+javac HomeTask1.java
