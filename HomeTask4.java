dd56b38a8332547329f67a109e234d1391b61145
Geork <112654321+jo777ko@users.noreply.github.com>
2022-09-25 13:41:11 +0200

    Add files via upload

--- HomeTask4.java
+++ HomeTask4.java
@@ -1,56 +1,37 @@
 import java.util.Random;
 import java.util.Scanner;
-import java.io.IOException;
+
 /**
  * Java Basic. HomeTask4
  * 
  * @authur Geork (Жора)
  * @todo 14.09.2022
- * @date 21.09.2022
+ * @date 22.09.2022
  */
 public class HomeTask4 {
-    public static char randomChar() {
-        Random random = new Random();
-        //Scanner scanner = new Scanner(System.in);
-        int rand = new Random().nextInt(52);
-        
-        char start = (rand < 26) ? 'A' : 'a';
-        return (char) (start + rand % 26);
-    }
- 
-    public static void main(String[] args) throws IOException {
-            char c = randomChar();
-            char ch = 'a';
-            System.out.print("Guess the symbol: ");
-            ch = (char)System.in.read();
-                if (c ==ch) {
-                    System.out.println("You guess");
-                } else {
-                    System.out.println("You don't guess");
-                }
+    public static void main (String[] args) {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-        int digit = 1;
-        int x = 0;
-        
+        int number = 0;
+        int guess = -1;
         do {
-            digit = random.nextInt(3);
-            for (int i = 0; i < 3; i++ ) {
-                System.out.print("Guess the digit [0...3]: ");
-                x = scanner.nextInt();
-                if (x > digit) {
-                    System.out.println("Your digit is higher");
-                } else if (x < digit) {
-                    System.out.println("Your digit is lesser");
+            number = random.nextInt(10);
+            for (int i = 0; i < 3; i++) {
+                System.out.print("Guess the number (0..9):");
+                guess = scanner.nextInt();
+                if (guess < number) {
+                    System.out.println("Your number is less");
+                } else if (guess > number) {
+                    System.out.println("Your number is bigger");
                 } else {
-                     System.out.println("You guess my digit");
-                     break;
+                    System.out.println("You guess my number");
+                    break;
                 }
             }
-            if (digit != x) {
-                 System.out.println("You lose");
+            if (number != guess) {
+                System.out.println("You lose.");
             }
-             System.out.println("Will you try again? Yes - 1, No - 0: ");
-        }while (scanner.nextInt() == 1);
+            System.out.println("Repeat game? Yes - 1, No -0 ");
+        } while (scanner.nextInt() == 1);
     }
 }
